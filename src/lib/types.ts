ff4ae96173d2f7b686ca8ba3262386fc1106191d
Ethan <ethanmiller13@outlook.com>
2024-06-10 19:00:41 -0230

    All of my stuff.

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+export type SortingAlgorithmType = 
+    "bubble"
+    "insertion"
+    "selection"
+    "merge"
+    "quick";
+
+  export type SelectOptionsType = {
+    value: string;
+    label: string;
+  };
+
+export type AnimationArrayType = [number[], boolean][];
